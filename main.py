95d175ec0c29e5ee069f7bdcd1694d3e0db0c56b
Angold-4 <wangold4w@gmail.com>
2022-08-08 02:57:50 +0800

    able to produce the correct output

--- main.py
+++ main.py
@@ -1,58 +1,33 @@
 import pickle
-import mnist_loader
-from image2minst import *
+import sys
 import numpy as np
 import cv2
 
+# load the trained model
 with open('recongnition_model.pkl', 'rb') as f:
     net = pickle.load(f)
 
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# total arguments
+n = len(sys.argv)
 
-n_test = len(test_data)
+if n != 2:
+    print("Usage: python3 main.py <your image>")
+    exit()
 
-test_img = cv2.imread("sample/7.png", cv2.IMREAD_GRAYSCALE)
+filename = sys.argv[1]
+
+test_img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
 sam_array = np.array(test_img).reshape((784, 1))
-
 new_array = []
-
 for i in range(len(sam_array)):
     new_array.append(sam_array[i,0]/255)
 
 input_sample = np.array(new_array).reshape((784, 1))
-
-print(input_sample)
-
-test_results = np.argmax(net.feedforward(input_sample))
 results = net.feedforward(input_sample)
+test_results = np.argmax(results)
 
-print(test_results)
-print(results)
+print("Recognized Value: {0}".format(test_results))
+value = results[test_results] * 100
+print("Confidence: " + format(value) + "%")
 
-
-"""
-test_results = np.argmax(net.feedforward(sam_array))
-results = net.feedforward(sam_array)
-
-print(test_results)
-print(results)
-
-"""
-"""
-print(array)
-
-sample_array = np.array(array).reshape((784, 1))
-
-test_results = np.argmax(net.feedforward(sample_array))
-results = net.feedforward(sample_array)
-
-pixels = sample_array.reshape(28, 28)
-plt.imshow(pixels, cmap='gray')
-plt.savefig('foo.png')
-
-print(test_results)
-print(results)
-"""
-
-print("{0} / {1}".format(net.evaluate(test_data), n_test))
